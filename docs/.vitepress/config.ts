a0401fbd699111c89a00780da821c1179926399a
Hara Keisuke <harakeisuke7@gmail.com>
2025-07-23 15:36:24 +0900

    基本私たちの物語と各種資料を分ける

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,30 +7,28 @@ export default defineConfig({
   themeConfig: {
     nav: [
       { text: 'ホーム', link: '/' },
-      { text: 'インセプションデッキ', link: '/inception-deck' },
-      { text: 'デモシナリオ', link: '/demo-scenario' },
-      { text: 'コード集', link: '/code-snippets' },
-      { text: 'Tips集', link: '/tips' },
-      { text: 'Q&A', link: '/q-and-a' } // ★Q&Aへのリンクを追加
+      { text: '開発ストーリー', link: '/development-story' },
+      { text: 'Tips集', link: '/tips' }
     ],
 
     sidebar: [
       {
-        text: 'プロジェクト資料',
+        text: '私たちの物語',
         collapsible: true,
         items: [
-          { text: 'インセプションデッキ', link: '/inception-deck' },
-          { text: 'デモシナリオ', link: '/demo-scenario' },
-          { text: 'コード集', link: '/code-snippets' },
-          { text: 'Tips集', link: '/tips' },
-          { text: 'Q&A', link: '/q-and-a' } // ★Q&Aへのリンクを追加
+          { text: '私たちが目指すもの', link: '/' },
+          { text: '設計思想 (Inception Deck)', link: '/inception-deck' },
+          { text: 'AIマーケットアナリスト開発記', link: '/development-story' },
+          { text: 'リファクタリング実践', link: '/refactoring' }, // ★リンクを修正
         ]
       },
       {
-        text: 'ケーススタディ',
+        text: '各種資料',
         collapsible: true,
         items: [
-          { text: 'AIマーケットアナリスト開発記', link: '/development-story' }
+          { text: 'コード集', link: '/code-snippets' },
+          { text: 'Tips集', link: '/tips' },
+          { text: 'Q&A', link: '/q-and-a' },
         ]
       }
     ]
